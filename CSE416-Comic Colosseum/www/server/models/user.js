f9fd1ee002aa7c9b58b4f815c18fe11cc3812338
teghpal-singh <59586085+teghpal-singh@users.noreply.github.com>
2022-03-16 12:08:42 -0400

    Delete CSE416-Comic Colosseum directory

--- CSE416-Comic Colosseum/www/server/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var UserSchema = new mongoose.Schema({
-  id: String,
-  username: String,
-  password: String,
-});
-
-module.exports = mongoose.model('User', UserSchema);
